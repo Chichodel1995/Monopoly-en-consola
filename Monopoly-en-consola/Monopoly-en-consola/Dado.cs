1ca031e5070a4062335b912bcb4e306b06137244
Chichodel1995 <>
2020-06-29 02:23:08 -0300

    Se agrego la Clase Dado

---
+++ Monopoly-en-consola/Monopoly-en-consola/Dado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly_en_consola
+{
+    public class Dado
+    {
+        private readonly Random dado1 = new Random();
+        private readonly Random dado2 = new Random();
+        private sbyte valorDado1 = 0;
+        private sbyte valorDado2 = 0;
+        public Dado(){}
+        public sbyte TirarDosDados()
+        {
+            valorDado1 = (sbyte)dado1.Next(1, 6);
+            valorDado2 = (sbyte)dado2.Next(1, 6);
+            return (sbyte)(valorDado1 + valorDado2);
+        }
+    }
+}
